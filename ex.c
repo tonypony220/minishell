ff4003a906d9a8b59c4ad526004fb82f4a29953f
Marq Ehtel <mehtel@am-c3.msk.21-school.ru>
2021-07-07 17:32:24 +0300

    cleared

--- ex.c
+++
@@ -1,9 +0,0 @@
-#include <unistd.h>
-int main()
-{
-	char* argv[] = { "jim", "jams", NULL };
-	char* envp[] = { "some", "environment", NULL };
-  	if (execve("jim", argv, envp) == -1)
-		perror("Could not execve");
-  return 1;
-}
